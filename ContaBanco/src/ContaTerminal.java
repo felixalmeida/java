645b0fc3f15202d5c87c5c35bc377ca2338ff54c
Felix Oliveira <felixoliveira89@gmail.com>
2023-09-19 09:12:41 -0300

    Criando método e objetos

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,27 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
+
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        
+        System.out.println("Digite o número da sua conta: ");
+        int numeroConta = scanner.nextInt();
+
+        System.out.println("Digite o número da sua agência: ");
+        String agencia = scanner.next();
+
+        System.out.println("Digite seu nome completo: ");
+        String nome = scanner.next();
+
+        System.out.println("Digite o valor inicial do primeiro depósito obrigatório: ");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("\"Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + " conta " + numeroConta + " e seu saldo " + saldo + " já está disponível para saque\".");
+
     }
+
+
 }
